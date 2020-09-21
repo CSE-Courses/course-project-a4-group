4516336255b8711f12051980e23caa81c369ed48
Kevin Lin <khlin@buffalo.edu>
2020-09-21 08:09:59 -0400

    connected template for Game Match Result to settings and views

--- GoodGames/gameMatchResult/views.py
+++ GoodGames/gameMatchResult/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def gameMatchResult(request):
-    return HttpResponse('<h1>Game Match Result</h1>')
+    return render(request, 'gameMatchResult/gameMatchResult.html')
