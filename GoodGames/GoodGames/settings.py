d8e9ff14ce6366b711f749e35db08d12e5c65a84
Kevin Lin <khlin@buffalo.edu>
2020-09-24 15:26:10 -0400

    #19 Applied Base Template to GameMatchResult page

--- GoodGames/GoodGames/settings.py
+++ GoodGames/GoodGames/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'game.apps.GameConfig',
-	  'gameMatchResult.apps.GamematchresultConfig',
+	'gameMatchResult.apps.GamematchresultConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
