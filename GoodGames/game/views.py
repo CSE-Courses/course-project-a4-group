5a0d8d231671e48bf2ad7be5d011f0c515e00402
Kevin Lin <khlin@buffalo.edu>
2020-10-17 21:04:49 -0400

    changed home to dashboard for new home page

--- GoodGames/game/views.py
+++ GoodGames/game/views.py
@@ -6,3 +6,5 @@ def home(request):
     return render(request, 'game/home.html')
 def shop(request):
     return render(request, 'game/shop.html')
+def dashboard(request):
+    return render(request, 'game/dashboard.html')
