b25fa08e900f5612e511fe27b397bc502aa8d5be
Kevin Lin <khlin@buffalo.edu>
2020-11-02 07:16:58 -0500

    added form POST success notification and redirection #51

--- GoodGames/gameMatchResult/views.py
+++ GoodGames/gameMatchResult/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import singleGameMatchResultForm
 from .models import singleGameMatchResult
 # Create your views here.
 
 def gameMatchResult(request):
-    form = singleGameMatchResultForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+    if request.method == 'POST':
+        form = singleGameMatchResultForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You have successfully submitted your game match result!')
+            return redirect('gameMatchResult-form')
+    else:
+        form = singleGameMatchResultForm()
     context = {
         'form': form
     }
